584667af77d4e3eb1f42487b47d5f499346c46aa
walterHeitor <walterheitorfo@gmail.com>
2020-07-14 22:51:51 -0300

    mapemento pedido e pagamento

--- src/main/java/com/softWalter/cursoMC/domain/Pedido.java
+++ src/main/java/com/softWalter/cursoMC/domain/Pedido.java
@@ -1,12 +1,25 @@
 package com.softWalter.cursoMC.domain;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Pedido {
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+@Entity
+public class Pedido implements Serializable{
 
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private Date  instane;
 	
+	@OneToOne(cascade = CascadeType.ALL, 
+			mappedBy = "pedido")
 	private Pagamento pagamento;
 	
 	private Cliente cliente;
