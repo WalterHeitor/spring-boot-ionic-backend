df8b02946047ceca0f72da0f9b9978e0120fb72b
walterHeitor <walterheitorfo@gmail.com>
2020-08-03 22:07:54 -0300

    mudou ? para Categoria

--- src/main/java/com/softWalter/cursoMC/resources/CategoriaResources.java
+++ src/main/java/com/softWalter/cursoMC/resources/CategoriaResources.java
@@ -21,7 +21,7 @@ public class CategoriaResources {
 	private CategoriaService service;
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET )
-	public ResponseEntity<?> find(@PathVariable Integer id){
+	public ResponseEntity<Categoria> find(@PathVariable Integer id){
 		
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
