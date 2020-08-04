94d04154b46ae13089f524ddc3fde42aa2a43e9c
walterHeitor <walterheitorfo@gmail.com>
2020-08-04 13:03:14 -0300

    findAll() correção

--- src/main/java/com/softWalter/cursoMC/resources/CategoriaResources.java
+++ src/main/java/com/softWalter/cursoMC/resources/CategoriaResources.java
@@ -2,6 +2,7 @@ package com.softWalter.cursoMC.resources;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.softWalter.cursoMC.domain.Categoria;
+import com.softWalter.cursoMC.dto.CategoriaDTO;
 import com.softWalter.cursoMC.services.CategoriaService;
 
 @RestController
@@ -50,10 +52,13 @@ public class CategoriaResources {
 		return ResponseEntity.noContent().build();
 	}
 	@RequestMapping(method = RequestMethod.GET )
-	public ResponseEntity<List<Categoria>> findAll(@PathVariable Integer id){
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
 		
 		List<Categoria> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<CategoriaDTO> listDto = list.stream().
+				map(obj -> new CategoriaDTO(obj)).
+				collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 
 	}
 }
