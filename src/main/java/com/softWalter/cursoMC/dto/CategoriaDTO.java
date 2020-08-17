eaef93e66a81178a7827352a24ced87e2cd6a96c
walterHeitor <walterheitorfo@gmail.com>
2020-08-17 19:41:06 -0300

    PUT, DELETE e GET cliente

--- src/main/java/com/softWalter/cursoMC/dto/CategoriaDTO.java
+++ src/main/java/com/softWalter/cursoMC/dto/CategoriaDTO.java
@@ -3,7 +3,6 @@ package com.softWalter.cursoMC.dto;
 import java.io.Serializable;
 
 import javax.validation.constraints.NotEmpty;
-
 import org.hibernate.validator.constraints.Length;
 
 import com.softWalter.cursoMC.domain.Categoria;
--- src/main/java/com/softWalter/cursoMC/services/ClienteService.java
+++ src/main/java/com/softWalter/cursoMC/services/ClienteService.java
@@ -1,12 +1,20 @@
 package com.softWalter.cursoMC.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.softWalter.cursoMC.domain.Categoria;
 import com.softWalter.cursoMC.domain.Cliente;
+import com.softWalter.cursoMC.dto.ClienteDTO;
 import com.softWalter.cursoMC.repositories.ClienteRepository;
+import com.softWalter.cursoMC.services.exceptions.DataIntegrityException;
 import com.softWalter.cursoMC.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,8 +26,48 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById(id);
 			return obj.orElseThrow(() -> new 
 					ObjectNotFoundException("Objeto não encontrado! Id: "
-					+ id + " ,Tipo: "+ Cliente.class.getName()));
-		
-		
+					+ id + " ,Tipo: "+ Cliente.class.getName()));		
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		find(obj.getId());
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não e possivel"
+					+ "excluir uma Cliente que contem produtos");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage,
+			String orderBy, String direction){
+		PageRequest  pageRequest = PageRequest.of(page, linesPerPage, 
+				Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(),
+				null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 }
