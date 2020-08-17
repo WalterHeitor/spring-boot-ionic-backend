8f3044b53ffde9f2cd207f64e5b8d83b38548697
walterHeitor <walterheitorfo@gmail.com>
2020-08-17 11:33:51 -0300

    validação da classe categoria dto

--- src/main/java/com/softWalter/cursoMC/dto/CategoriaDTO.java
+++ src/main/java/com/softWalter/cursoMC/dto/CategoriaDTO.java
@@ -2,11 +2,19 @@ package com.softWalter.cursoMC.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.softWalter.cursoMC.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigátorio.")
+	@Length(min = 5, max = 80, message = "Otamanho deve ser entre"
+			+ "5 e 80 caracteres")
 	private String nome;
 	public CategoriaDTO() {
 		super();
--- src/main/java/com/softWalter/cursoMC/services/CategoriaService.java
+++ src/main/java/com/softWalter/cursoMC/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.softWalter.cursoMC.domain.Categoria;
+import com.softWalter.cursoMC.dto.CategoriaDTO;
 import com.softWalter.cursoMC.repositories.CategoriaRepository;
 import com.softWalter.cursoMC.services.exceptions.DataIntegrityException;
 import com.softWalter.cursoMC.services.exceptions.ObjectNotFoundException;
@@ -58,6 +59,10 @@ public class CategoriaService {
 		PageRequest  pageRequest = PageRequest.of(page, linesPerPage, 
 				Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
