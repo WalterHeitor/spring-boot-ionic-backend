7ddaba60dcac991e50e12c5a688f7f66c0fd04ba
walterHeitor <walterheitorfo@gmail.com>
2020-08-19 16:37:39 -0300

    teste de validação do cliente

--- src/main/java/com/softWalter/cursoMC/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/softWalter/cursoMC/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.softWalter.cursoMC.domain.Cliente;
 import com.softWalter.cursoMC.domain.enums.TipoCliente;
 import com.softWalter.cursoMC.dto.ClienteNewDTO;
+import com.softWalter.cursoMC.repositories.ClienteRepository;
 import com.softWalter.cursoMC.resources.exceptions.FieldMessage;
 import com.softWalter.cursoMC.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository ClienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -30,6 +38,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod())
 				&& !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add( new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = ClienteRepository.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já exitente."));
+			
 		}
 
 		for (FieldMessage e : list) {
